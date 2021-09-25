cf8e5f9ea957142f6bb92cf94e07cc4be09943e1
carla2807 <cm90_mdp@hotmail.com>
2021-09-25 16:10:19 -0300

    Fin de la app

---
+++ src/app/gifs/resultados/resultados.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { GifsService } from '../services/gifs.service';
+
+@Component({
+  selector: 'app-resultados',
+  templateUrl: './resultados.component.html',
+  styles: [],
+})
+export class ResultadosComponent {
+  constructor(private GifsService: GifsService) {}
+
+  get resultados() {
+    return this.GifsService.resultados;
+  }
+}
